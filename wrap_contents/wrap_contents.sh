f1dffd0a3313b231ae634a5bb9ba7ac4bd4f86f9
burde041 <burde041@morris.umn.edu>
2021-09-07 20:12:14 -0500

    Fix wrap_contents according to shellcheck

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Wraps given file contents between header and footer then puts it in new file
-cat $2_header.html $1 $2_footer.html > $3
+cat "$2"_header.html "$1" "$2"_footer.html > "$3"
